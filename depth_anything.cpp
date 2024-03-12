6d7dd0c526e57a3dad246d5b26ce33042e876ab2
spacewalk01 <selmee006@gmail.com>
2024-03-12 09:14:20 +0900

    Update depth_anything.cpp

--- depth_anything.cpp
+++ depth_anything.cpp
@@ -1,14 +1,8 @@
 #include "depth_anything.h"
-
 #include <NvOnnxParser.h>
 
-// set network params
-float DepthAnything::input_h = 518;
-float DepthAnything::input_w = 518;
-int DepthAnything::num_classes = 3;
-float DepthAnything::mean[3] = { 123.675, 116.28, 103.53 };
-float DepthAnything::std[3] = { 58.395, 57.12, 57.375 };
-static bool isFP16 = true;
+#define isFP16 true
+
 using namespace nvinfer1;
 
 /**
@@ -34,9 +28,6 @@ DepthAnything::DepthAnything(std::string model_path, nvinfer1::ILogger& logger)
         runtime = nvinfer1::createInferRuntime(logger);
         engine = runtime->deserializeCudaEngine(engineData.get(), modelSize);
         context = engine->createExecutionContext();
-
-        // Define input dimensions
-        context->setBindingDimensions(0, nvinfer1::Dims4(1, 3, input_h, input_w));
     }
     // Build an engine from an onnx model
     else
@@ -44,6 +35,11 @@ DepthAnything::DepthAnything(std::string model_path, nvinfer1::ILogger& logger)
         build(model_path, logger);
         saveEngine(model_path);
     }
+
+    // Define input dimensions
+    auto input_dims = engine->getBindingDimensions(0);
+    input_h = input_dims.d[2];
+    input_w = input_dims.d[3];
 
     // create CUDA stream
     cudaStreamCreate(&stream);
@@ -61,34 +57,6 @@ DepthAnything::~DepthAnything()
     cudaFree(buffer[0]);
     cudaFree(buffer[1]);
 }
-
-
-/**
- * @brief Display network input and output parameters
-*/
-void DepthAnything::show()
-{
-    for (int i = 0; i < engine->getNbBindings(); i++)
-    {
-        std::cout << "node: " << engine->getBindingName(i) << ", ";
-        if (engine->bindingIsInput(i))
-        {
-            std::cout << "type: input" << ", ";
-        }
-        else
-        {
-            std::cout << "type: output" << ", ";
-        }
-        nvinfer1::Dims dim = engine->getBindingDimensions(i);
-        std::cout << "dimensions: ";
-        for (int d = 0; d < dim.nbDims; d++)
-        {
-            std::cout << dim.d[d] << " ";
-        }
-        std::cout << "\n";
-    }
-}
-
 
 /**
  * @brief Network preprocessing function
@@ -134,21 +102,12 @@ cv::Mat DepthAnything::predict(cv::Mat& image)
     cudaMemcpyAsync(depth_data.data(), buffer[1], input_h * input_w * sizeof(int), cudaMemcpyDeviceToHost);
 
     // Convert the entire depth_data vector to a CV_32FC1 Mat
-    cv::Mat mask_mat(input_h, input_w, CV_32FC1);
-    for (int i = 0; i < input_h; i++) {
-        for (int j = 0; j < input_w; j++) {
-            // Convert integer value to float
-            float float_value = *reinterpret_cast<float*>(&depth_data[i * input_w + j]);
-
-            // Assign the float value to the corresponding pixel in the Mat
-            mask_mat.at<float>(i, j) = float_value;
-        }
-    }
-    cv::normalize(mask_mat, mask_mat, 0, 255, cv::NORM_MINMAX, CV_8U);
+    cv::Mat depth_mat(input_h, input_w, CV_32FC1, depth_data.data());
+    cv::normalize(depth_mat, depth_mat, 0, 255, cv::NORM_MINMAX, CV_8U);
 
     // Create a colormap from the depth data
     cv::Mat colormap;
-    cv::applyColorMap(mask_mat, colormap, cv::COLORMAP_INFERNO);
+    cv::applyColorMap(depth_mat, colormap, cv::COLORMAP_INFERNO);
 
     // Rescale the colormap
     int limX, limY;
